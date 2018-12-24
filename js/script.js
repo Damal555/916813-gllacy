e158a47b89e69747db0d76c9492ce5f45be9a5dd
Damal555 <44976754+Damal555@users.noreply.github.com>
2018-12-24 20:57:24 +0100

    Финальные правки перед предзащитой

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ try {
   isStorageSupport = false;
 }
 
-link.addEventListener("click", function (evt) {
+link.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
 
@@ -28,13 +28,13 @@ link.addEventListener("click", function (evt) {
   }
 });
 
-close.addEventListener("click", function (evt) {
+close.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
 });
 
-form.addEventListener("submit", function (evt) {
+form.addEventListener("submit", function(evt) {
   if (!login.value || !email.value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
@@ -47,7 +47,7 @@ form.addEventListener("submit", function (evt) {
   }
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
